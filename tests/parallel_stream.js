f2daadd06db34c5563837d2ba2c072cee696a645
Fumiya <uzuna.kf@gmail.com>
2016-12-17 08:56:23 +0900

    [Test]Add variable packet

--- tests/parallel_stream.js
+++ tests/parallel_stream.js
@@ -10,21 +10,6 @@ const Parallel = require('../lib/parallel_stream')
 const expect = require('chai').expect
 
 describe('parallel', function(){
-	it('test run function', function(){
-		return aa(main())
-		function *main(){
-			let fn = function(){}
-			let g = function *(){}
-			// console.log(fn.constructor.name, g.constructor.name)
-
-			let dest = Parallel.parallel({max:3,run:function(){
-				// console.log(this)
-				this.push("AAA")
-			}})
-			dest._run()
-		}
-	})
-
 	it("transform function", function(){
 		return aa(main())
 		function *main(){
@@ -64,6 +49,56 @@ describe('parallel', function(){
 				}
 			}
 			const src = au.stream.g2s(spt.range(0,10)())
+			const dest = Parallel.parallel(opt)
+			const latest = au.stream.pipe([src, dest])
+			const row = yield au.stream.reduce(latest)
+			expect(row).to.be.a('array')
+			row.forEach((d)=>{
+				expect(d % 2).to.be.eq(0)
+			})
+			// console.log(row)
+		}
+	})
+
+	it("short packet",function(){
+		return aa(main())
+		function *main(){
+			const opt = {
+				objectMode: true,
+				max:10,
+				run: function*(chunk, enc){
+					// console.log(chunk)
+					yield spt.sleep(100)
+					chunk = chunk * 2
+					this.push(chunk)
+				}
+			}
+			const src = au.stream.g2s(spt.range(0,1)())
+			const dest = Parallel.parallel(opt)
+			const latest = au.stream.pipe([src, dest])
+			const row = yield au.stream.reduce(latest)
+			expect(row).to.be.a('array')
+			row.forEach((d)=>{
+				expect(d % 2).to.be.eq(0)
+			})
+			// console.log(row)
+		}
+	})
+
+	it("long packet",function(){
+		return aa(main())
+		function *main(){
+			const opt = {
+				objectMode: true,
+				max:100,
+				run: function*(chunk, enc){
+					// console.log(chunk)
+					yield spt.sleep(10)
+					chunk = chunk * 2
+					this.push(chunk)
+				}
+			}
+			const src = au.stream.g2s(spt.range(0,500)())
 			const dest = Parallel.parallel(opt)
 			const latest = au.stream.pipe([src, dest])
 			const row = yield au.stream.reduce(latest)
